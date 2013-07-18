5fc0d334190b02a18c5aafc6bd7484be3ed630d3
Mark Ridgwell <github@markridgwell.com>
2013-07-18 20:49:17 +0100

    Added command line and nupkg to gitignore

--- build.cmd
+++ build.cmd
@@ -1,1 +1,5 @@
-nuget pack -build Twaddle.Web.Mvc.Extensions.csproj -Prop Configuration=Release
+rem nuget spec
+
+nuget pack -build Twaddle.Web.Mvc.Extensions.csproj -Prop Configuration=Release -IncludeReferencedProjects
+
+rem nuget push Twaddle.Web.Mvc.Extensions.{version}.nupkg
